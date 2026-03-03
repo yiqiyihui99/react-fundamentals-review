be90dc1893364e7896d6f36d0cefa2ba94fa4687
yiqiyihui99 <ianwooduffy.99@gmail.com>
2026-03-03 00:14:01 -0800

    Added new custom hook to fetch pizza of the day AND react StrictMode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 
 const App = () => {
   return (
-    <div>
-      <h1>Padre Gino's - Order Now</h1>
-      <Order />
-    </div>
+    <StrictMode>
+      <div>
+        <h1>Padre Gino's - Order Now</h1>
+        <Order />
+        <PizzaOfTheDay />
+      </div>
+    </StrictMode>
   );
 };
 
---
+++ src/usePizzaOfTheDay.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export const usePizzaOfTheDay = () => {
+  const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+
+  useEffect(() => {
+    async function fetchPizzaOfTheDay() {
+      const response = await fetch("/api/pizza-of-the-day");
+      const data = await response.json();
+      setPizzaOfTheDay(data);
+    }
+
+    fetchPizzaOfTheDay();
+  }, []);
+
+  return pizzaOfTheDay;
+};
