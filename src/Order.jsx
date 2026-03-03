1f7003f78d8464964a802869f5d8f4c48798f163
yiqiyihui99 <ianwooduffy.99@gmail.com>
2026-03-03 10:27:50 -0800

    Added checkout function for cart

--- src/Order.jsx
+++ src/Order.jsx
@@ -14,6 +14,20 @@ export default function Order() {
   const [cart, setCart] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  async function checkout() {
+    setLoading(true);
+    await fetch("/api/order", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ cart }),
+    });
+
+    setCart([]);
+    setLoading(false);
+  }
+
   let price, selectedPizza;
 
   if (!loading) {
@@ -114,7 +128,11 @@ export default function Order() {
           )}
         </form>
       </div>
-      {loading ? <h2>LOADING ...</h2> : <Cart cart={cart} />}
+      {loading ? (
+        <h2>LOADING ...</h2>
+      ) : (
+        <Cart cart={cart} checkout={checkout} />
+      )}
     </div>
   );
 }
