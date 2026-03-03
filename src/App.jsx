ae7807fb8daed5477a117968f1fc2afcf90b20e9
yiqiyihui99 <ianwooduffy.99@gmail.com>
2026-03-03 00:30:28 -0800

    nit: Adjusted stlying, added useDebugValue for practice

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <StrictMode>
       <div>
-        <h1>Padre Gino's - Order Now</h1>
+        <h1 className="logo">Padre Gino's - Order Now</h1>
         <Order />
         <PizzaOfTheDay />
       </div>
--- src/usePizzaOfTheDay.jsx
+++ src/usePizzaOfTheDay.jsx
@@ -1,7 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useDebugValue } from "react";
 
 export const usePizzaOfTheDay = () => {
   const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+  useDebugValue(
+    //Helps debug custom hooks
+    pizzaOfTheDay ? `${pizzaOfTheDay.id} : ${pizzaOfTheDay.name}` : "loading",
+  );
 
   useEffect(() => {
     async function fetchPizzaOfTheDay() {
